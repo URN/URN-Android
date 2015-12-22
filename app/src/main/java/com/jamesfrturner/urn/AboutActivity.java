41002ad3de4ab9594e46300cf0435022c529d778
James Turner <me@jamesturner.im>
2015-12-22 14:24:58 +0000

    Populate about page

--- app/src/main/java/com/jamesfrturner/urn/AboutActivity.java
+++ app/src/main/java/com/jamesfrturner/urn/AboutActivity.java
@@ -1,8 +1,12 @@
 package com.jamesfrturner.urn;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
     @Override
@@ -10,6 +14,13 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_about);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final Button button = (Button) findViewById(R.id.openWebsiteBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://urn1350.net")));
+            }
+        });
 
     }
 }
