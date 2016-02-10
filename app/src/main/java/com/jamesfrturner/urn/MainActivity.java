906517cfd15e2c913c52dd69f3536e21adf08a4b
James Turner <me@jamesturner.im>
2016-02-10 00:00:39 +0000

    Add basic colors, strings and menu items to app

--- app/src/main/java/com/jamesfrturner/urn/MainActivity.java
+++ app/src/main/java/com/jamesfrturner/urn/MainActivity.java
@@ -2,6 +2,7 @@ package com.jamesfrturner.urn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
