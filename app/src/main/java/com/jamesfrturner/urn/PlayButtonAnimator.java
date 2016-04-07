422d66279f391a87ec192ace4ce74f7da45bf2a2
James Turner <me@jamesturner.im>
2016-04-08 00:02:09 +0100

    Add  animation to wings around play button

--- app/src/main/java/com/jamesfrturner/urn/PlayButtonAnimator.java
+++ app/src/main/java/com/jamesfrturner/urn/PlayButtonAnimator.java
@@ -3,7 +3,11 @@ package com.jamesfrturner.urn;
 import android.app.Activity;
 import android.content.Context;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class PlayButtonAnimator {
     public final static int STATE_STOPPED = 0;
@@ -15,12 +19,19 @@ public class PlayButtonAnimator {
     private Activity activity;
 
     private Button playButton;
+    private ImageView topWings;
+    private ImageView bottomWings;
+
+    private Animation spinAnimation;
 
     public PlayButtonAnimator(Context context, Activity activity) {
         this.context=context;
         this.activity=activity;
 
         playButton = (Button) activity.findViewById(R.id.play_button);
+        topWings = (ImageView) activity.findViewById(R.id.play_wings_top);
+        bottomWings = (ImageView) activity.findViewById(R.id.play_wings_bottom);
+        spinAnimation = AnimationUtils.loadAnimation(context, R.anim.scale_play_button_wings);
     }
 
     public void changeState(int newState) throws Exception {
@@ -85,10 +96,17 @@ public class PlayButtonAnimator {
 
     private void startPlayAnimation() {
         playButton.setBackgroundResource(R.drawable.circle_stop_button);
+        topWings.startAnimation(spinAnimation);
+        bottomWings.startAnimation(spinAnimation);
+        topWings.setVisibility(View.VISIBLE);
+        bottomWings.setVisibility(View.VISIBLE);
     }
 
     private void stopPlayAnimation() {
-
+        topWings.clearAnimation();
+        bottomWings.clearAnimation();
+        topWings.setVisibility(View.INVISIBLE);
+        bottomWings.setVisibility(View.INVISIBLE);
     }
 
     private void startStopAnimation() {
